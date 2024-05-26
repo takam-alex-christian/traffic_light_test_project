351612e9580de1b161a56b2b2c281258741c8f4e
Takam Alex Christian <takamalexchristian@gmail.com>
2024-05-26 14:43:37 +0000

    added blinking to each light at the end of a cycle.

--- main.cpp
+++ main.cpp
@@ -1,40 +1,40 @@
 #include "mbed.h"
 
-
 DigitalOut red(PA_5);
 DigitalOut green(PA_6);
 DigitalOut yellow(PA_7);
 
-
-void dimAllLeds(DigitalOut *leds){
-    for(size_t i = 0; i <= 2; i++){
-        leds[i] = false;
-    }
+void dimAllLeds(DigitalOut *leds) {
+  for (size_t i = 0; i <= 2; i++) {
+    leds[i] = false;
+  }
 }
 
 // main() runs in its own thread in the OS
 int main() {
-    
-    DigitalOut leds[3] = {red, green, yellow};
 
-    int counter = 0;
+  DigitalOut leds[3] = {red, green, yellow};
 
-    while (true){
+  int counter = 0;
 
-        dimAllLeds(leds);
+  while (true) {
 
-        if (counter <= 2){
-            
-            leds[counter] = true;
+    dimAllLeds(leds);
 
-            counter++;
-        }else {
-            counter = 0;
-        }
+    if (counter <= 2) {
 
-        
+      leds[counter] = true;
 
-        ThisThread::sleep_for(400ms);
-        
+      ThisThread::sleep_for(2000ms);
+
+      for (size_t i = 0; i <= 10; i++){
+          leds[counter] = !leds[counter];
+          ThisThread::sleep_for(400ms);
+      }
+
+        counter++;
+    } else {
+      counter = 0;
     }
+  }
 }
